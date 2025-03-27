kawabart/CircleSurvival

commit 560941f03715c6ac197e578406f1b3d4b1f35da6

Fix spawn field on different resolutions

Assets/Scripts/CircleSpawner.cs
@@ -14,14 +14,17 @@ public class CircleSpawner : MonoBehaviour
     private List<Vector2> spawnPositions = new List<Vector2>();
     private float SpawnDelay { get => 3 / (game.Score - (-1f)) + 0.2f; }
     private float MinLifespanGreen { get => 100f / (game.Score - (-66f)) + 0.5f; }
+    private Vector2 resolutionScale;
 
     private void Start()
     {
+        resolutionScale = canvas.GetComponent<RectTransform>().localScale;
+
         for (int i = 0; i < parent.childCount; i++)
         {
             var circle = parent.GetChild(i);
             Vector2 position = circle.GetComponent<RectTransform>().position;
-            spawnPositions.Add(position);
+            spawnPositions.Add(position * resolutionScale);
             StartCoroutine(ClearSpawnPosition(position, circle.GetComponent<Circle>().Lifespan));
         }
     }
@@ -33,8 +36,8 @@ public class CircleSpawner : MonoBehaviour
         {
             GameObject circlePrefab = Random.Range(0, 10) > 0 ? circleGreenPrefab : circleBlackPrefab;
             Vector2 circleSize = circlePrefab.GetComponent<RectTransform>().rect.size;
-            float posX = Random.Range(0 + circleSize.x / 2, canvas.rect.width - circleSize.x / 2);
-            float posY = Random.Range(0 + circleSize.y / 2, canvas.rect.height - HUD.rect.height - circleSize.y / 2);
+            float posX = Random.Range(0 + circleSize.x / 2, canvas.rect.width - circleSize.x / 2) * resolutionScale.x;
+            float posY = Random.Range(0 + circleSize.y / 2, canvas.rect.height - HUD.rect.height - circleSize.y / 2) * resolutionScale.y;
             Vector3 newPosition = new Vector2(posX, posY);
 
             foreach (Vector2 position in spawnPositions)

Assets/Scenes/Gameplay.unity
@@ -947,7 +947,6 @@ MonoBehaviour:
   parent: {fileID: 585530763}
   canvas: {fileID: 1949668305}
   HUD: {fileID: 700816527}
-  spawnDelay: 3
 --- !u!4 &1650282839
 Transform:
   m_ObjectHideFlags: 0
@@ -1165,7 +1164,7 @@ MonoBehaviour:
   m_ScaleFactor: 1
   m_ReferenceResolution: {x: 1080, y: 1920}
   m_ScreenMatchMode: 0
-  m_MatchWidthOrHeight: 0
+  m_MatchWidthOrHeight: 0.5
   m_PhysicalUnit: 3
   m_FallbackScreenDPI: 96
   m_DefaultSpriteDPI: 96

ProjectSettings/ProjectSettings.asset
@@ -118,7 +118,7 @@ PlayerSettings:
     16:10: 1
     16:9: 1
     Others: 1
-  bundleVersion: 1.1
+  bundleVersion: 1.1.2
   preloadedAssets: []
   metroInputSource: 0
   wsaTransparentSwapchain: 0

commit 3af9328b27edc9ba05bbee397b189f79f9150467

Fix spawning circles that overlap initial circles
Could have occurred during the first 3 seconds (default circles lifespan)
when the random new circle position was close enough
to the initial circles in the middle,
and only at non-default resolution of canvas (different than 1080x1920).

Also was causing another hard-to-find issue
‒ the initial circles positions, mistakenly multiplied by resolution scale,
were not erased from the spawn positions list,
which resulted in circles not being able to be created at those positions.

The bug was introduced in the commit 560941f.

Assets/Scripts/CircleSpawner.cs
@@ -24,7 +24,7 @@ public class CircleSpawner : MonoBehaviour
         {
             var circle = parent.GetChild(i);
             Vector2 position = circle.GetComponent<RectTransform>().position;
-            spawnPositions.Add(position * resolutionScale);
+            spawnPositions.Add(position);
             StartCoroutine(ClearSpawnPosition(position, circle.GetComponent<Circle>().Lifespan));
         }
     }